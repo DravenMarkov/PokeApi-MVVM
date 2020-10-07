57e9b9dce194088d2b1269ccdd97123013f89018
Zenemij <ti.ericjimenez@gmail.com>
2020-10-07 19:56:47 +0200

    adding mapper

---
+++ app/src/main/java/com/example/pokeapi/domain/mapper/ResultsMapper.kt
@@ -0,0 +1,21 @@
+package com.example.pokeapi.domain.mapper
+
+import com.example.pokeapi.data.entity.PokedexData
+import com.example.pokeapi.domain.entity.PokedexEntity
+
+class ResultsMapper {
+
+    companion object {
+
+        fun convert(listPokemon: List<PokedexData.Result>) : List<PokedexEntity.Result> =
+            listPokemon.map { convert(it) }
+
+
+        fun convert(model: PokedexData.Result): PokedexEntity.Result = with(model){
+            PokedexEntity.Result(
+                name = name,
+                url = url
+            )
+        }
+    }
+}
